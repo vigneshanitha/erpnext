fd2c913c392c0e57f1d89709679d735d30c1cf1b
vigneshanitha <vikkyprashad@gmail.com>
2020-11-07 00:24:16 +0300

    Update expense_claim.py
    I have made the changes in line 221 since this affects the total claimed amount is not updating when adding the taxes and other charges. It also contradicts with the if condition at line number 249.

--- erpnext/hr/doctype/expense_claim/expense_claim.py
+++ erpnext/hr/doctype/expense_claim/expense_claim.py
@@ -221,6 +221,7 @@ class ExpenseClaim(AccountsController):
 			self.total_taxes_and_charges += flt(tax.tax_amount)
 
 		self.grand_total = flt(self.total_sanctioned_amount) + flt(self.total_taxes_and_charges) - flt(self.total_advance_amount)
+		self.total_claimed_amount += flt(self.grand_total)
 
 	def update_task(self):
 		task = frappe.get_doc("Task", self.task)
